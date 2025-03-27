89d97e2922b6a007ed1f682dddbcacc9efc062fe
hajatianaratrimoson <hajatiana.ratrimoson@gmail.com>
2025-03-27 08:28:40 +0300

    first settings conf

--- fjkmtvf/fjkmtvf/settings.py
+++ fjkmtvf/fjkmtvf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-+11d=r0j4(d-s+)5i8*ftxeo582(inidl!ppy1xcervu@ip-qz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fjkmtvf.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
